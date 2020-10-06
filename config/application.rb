edebab6e718358b4d044a876d0ca8921a4830f60
Agat Hirachan <ahirachan@maharam.com>
2020-10-06 09:47:56 -0400

    Deployment filed potential fix

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,6 @@ module Myapp
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     # config/application.rb
-    config.assets.initialize_on_precompile = false
+    #config.assets.initialize_on_precompile = false
   end
 end
