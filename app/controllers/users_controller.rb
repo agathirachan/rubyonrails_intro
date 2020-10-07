2bb508cdff2a6a9c769a5a0a89687536b8e859f4
Agat Hirachan <ahirachan@maharam.com>
2020-10-07 12:55:52 -0400

    Signup  create completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,24 @@ class UsersController < ApplicationController
     end
 
     def create
+        @user = User.new(user_params)
+        if @user.save
+          flash[:notice] ="User was successfully added."
+          redirect_to articles_path
+        else
+          render 'new'
+        end
     end
+     # anything below private is a private method
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    # byebug
+    @user = Article.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
